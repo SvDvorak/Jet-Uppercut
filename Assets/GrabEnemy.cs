c30c0531cd5c58115375433461fd0b984b94c8a3
Andreas Wilcox <unknownbum@gmail.com>
2017-08-25 16:24:42 +0200

    Made basic jump functionality

---
+++ Assets/GrabEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GrabEnemy : MonoBehaviour
+{
+    public Transform Offset;
+
+    private FixedJoint2D _grabJoint;
+    private bool _isGrabbing;
+    private Transform _lastGrabbed;
+    
+    public void Start()
+    {
+        _grabJoint = GetComponent<FixedJoint2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && collision.transform != _lastGrabbed)
+        {
+            _grabJoint.enabled = true;
+            _grabJoint.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            transform.position = collision.transform.position - Offset.transform.localPosition;
+            _lastGrabbed = collision.transform;
+            _isGrabbing = true;
+        }
+    }
+
+    public void Disconnect()
+    {
+        if (_isGrabbing)
+        {
+            _isGrabbing = false;
+
+            _grabJoint.enabled = false;
+            _grabJoint.connectedBody = null;
+
+            StartCoroutine(ResetLastGrabbed());
+        }
+    }
+
+    public IEnumerator ResetLastGrabbed()
+    {
+        yield return new WaitForSeconds(1);
+        if(!_isGrabbing)
+        {
+            _lastGrabbed = null;
+        }
+    }
+}
---
+++ Assets/Jump.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float Force;
+
+    private Vector3 _inputDownPosition;
+    private Rigidbody2D _rigidbody;
+
+    public void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _inputDownPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(0) && _inputDownPosition != null)
+        {
+            GetComponent<GrabEnemy>().Disconnect();
+
+            var inputEndPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            var jumpDirection = (inputEndPosition - _inputDownPosition).normalized;
+
+            _rigidbody.AddForce(jumpDirection*Force, ForceMode2D.Impulse);
+        }
+    }
+}
